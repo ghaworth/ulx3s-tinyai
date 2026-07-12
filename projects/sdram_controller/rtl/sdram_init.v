0aefcc7ec1c3225370aa416d01c4452b95678b79
George Haworth <info@robust-ae.com>
2026-07-12 21:15:44 +0100

    Add datasheet timing constants and ns-to-cycles conversion

--- projects/sdram_controller/rtl/sdram_init.v
+++ projects/sdram_controller/rtl/sdram_init.v
@@ -1,5 +1,5 @@
 module sdram_init #(
-	parameter CLK_FREQ_HZ = 100000000,
+	parameter CLK_FREQ_HZ = 100_000_000,
 	parameter CAS_LATENCY = 2
 )(
 	input wire clk,
@@ -7,3 +7,14 @@ module sdram_init #(
 	output reg done
 );
 
+localparam tRP_NS = 15;                 		// Winbond -6, §9.5
+localparam tRC_NS = 60;     		    		// ...
+localparam tRAS_NS = 42; 						// ...
+localparam tRCD_NS = 15; 						// ...
+localparam POWERUP_PAUSE_NS = 200_000;	// Winbond -6, §7.1
+localparam AUTO_REFRESH_COUNT = 8;     			// ...
+localparam tRP_CYCLES = (64'd1 * tRP_NS  * CLK_FREQ_HZ + 999_999_999) / 1_000_000_000;
+localparam tRC_CYCLES = (64'd1 * tRC_NS  * CLK_FREQ_HZ + 999_999_999) / 1_000_000_000;
+localparam tRAS_CYCLES = (64'd1 * tRAS_NS  * CLK_FREQ_HZ + 999_999_999) / 1_000_000_000;
+localparam tRCD_CYCLES = (64'd1 * tRCD_NS  * CLK_FREQ_HZ + 999_999_999) / 1_000_000_000;
+localparam POWERUP_PAUSE_CYCLES = (64'd1 * POWERUP_PAUSE_NS  * CLK_FREQ_HZ + 999_999_999) / 1_000_000_000;
